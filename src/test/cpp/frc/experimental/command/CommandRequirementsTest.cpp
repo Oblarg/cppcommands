77848de2cc78755008e9d9a103934e4f9b1898d6
Eli <emichaelbarnett@gmail.com>
2019-06-19 22:02:32 -0400

    add requirementinterrupttest

---
+++ src/test/cpp/frc/experimental/command/CommandRequirementsTest.cpp
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+#include "frc/experimental/command/CommandScheduler.h"
+#include "frc/experimental/buttons/InternalButton.h"
+#include "CommandTestBase.h"
+
+using namespace frc::experimental;
+
+class CommandRequirementsTest : public CommandTestBase {
+  
+};
+
+TEST_F(CommandRequirementsTest, RequirementInterruptTest) {
+  CommandScheduler scheduler = GetScheduler();
+
+  TestSubsystem* requirement = new TestSubsystem();
+
+  MockCommandHolder command1Holder{true, {requirement}};
+  MockCommandHolder::MockCommand* command1 = command1Holder.GetMock();
+  MockCommandHolder command2Holder{true, {requirement}};
+  MockCommandHolder::MockCommand* command2 = command2Holder.GetMock();
+
+  EXPECT_CALL(*command1, Initialize());
+  EXPECT_CALL(*command1, Execute());
+  EXPECT_CALL(*command1, End(true));
+  EXPECT_CALL(*command1, End(false)).Times(0);
+
+  EXPECT_CALL(*command2, Initialize());
+  EXPECT_CALL(*command2, Execute());
+  EXPECT_CALL(*command2, End(true)).Times(0);
+  EXPECT_CALL(*command2, End(false)).Times(0);
+
+  scheduler.Schedule(command1);
+  scheduler.Run();
+  EXPECT_TRUE(scheduler.IsScheduled(command1));
+  scheduler.Schedule(command2);
+  EXPECT_FALSE(scheduler.IsScheduled(command1));
+  EXPECT_TRUE(scheduler.IsScheduled(command2));
+  scheduler.Run();
+}
