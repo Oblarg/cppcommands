185ecf687fc8dd23e06c1c3bf2d83e15f96d9e0a
carbotaniuman <quy.d.x.nguyen@gmail.com>
2019-07-09 10:50:05 -0500

    Prototype

---
+++ src/main/include/frc/experimental/command/CommandHelper.h
@@ -0,0 +1,18 @@
+#include <type_traits>
+
+#include "Command.h"
+
+namespace frc {
+namespace experimental {
+
+template<typename Base, typename CRTP, typename = std::enable_if_t<std::is_base_of_v<Command, Base>>>
+class CommandHelper: public Base {
+    using Base::Base;
+
+protected:
+    std::unique_ptr<Command> TransferOwnership()&& override {
+        return std::make_unique<CRTP>(std::move(*static_cast<CRTP*>(this)));
+    }
+};
+}
+}
--- src/main/include/frc/experimental/command/RunCommand.h
+++ src/main/include/frc/experimental/command/RunCommand.h
@@ -1,10 +1,11 @@
 #pragma once
 
+#include "CommandHelper.h"
 #include "SendableCommandBase.h"
 
 namespace frc {
 namespace experimental {
-class RunCommand : public SendableCommandBase {
+class RunCommand : public CommandHelper<SendableCommandBase, RunCommand> {
  public:
   RunCommand(std::function<void()> toRun, wpi::ArrayRef<Subsystem*> requirements) 
     : m_toRun{std::move(toRun)} {
@@ -17,10 +18,6 @@ class RunCommand : public SendableCommandBase {
     m_toRun();
   }
  protected:
-  protected:
-  std::unique_ptr<Command> TransferOwnership()&& override {
-    return std::make_unique<RunCommand>(std::move(*this));
-  }
   std::function<void()> m_toRun;
 };
 }
